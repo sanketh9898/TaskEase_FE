d5f02bb667e9cf6f9cc5d61e3b5fd5108313a1a9
sanketh9898 <49584673+sanketh9898@users.noreply.github.com>
2025-04-07 23:12:56 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts (Development)
 export const environment = {
     production: false,
-    apiUrl: 'http://10.0.58.130:8081/api' // Your Spring Boot backend URL
+    apiUrl: 'http://taskease-ALB-296566271.us-east-1.elb.amazonaws.com:8081/api' // Your Spring Boot backend URL
   };
